2e79c7d0d409759ed2ea88e2bdbf5f3336842a34
James Mason <jmason@kloveair1.com>
2015-11-09 15:26:04 -0800

    01. A simple Transfer service

---
+++ app/services/transfer.rb
@@ -0,0 +1,11 @@
+class Transfer
+  def call(amount:, from:, to:)
+    Account.transaction do
+      from.balance -= amount
+      to.balance += amount
+
+      from.save!
+      to.save!
+    end
+  end
+end
