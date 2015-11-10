4a249e21695f7a754eb625727cc37991754251dc
James Mason <jmason@kloveair1.com>
2015-11-09 16:31:03 -0800

    05. Refactor using an internal object
    Another option for refactoring is using an internal object. This is
    especially good when you have a lot of parameters that must be
    passed to your private methods. A internal object lets you save state
    that only lasts for the lifetime of the #call method.

--- app/services/internal_transfer.rb
+++ app/services/internal_transfer.rb
@@ -1,19 +1,27 @@
 class InternalTransfer
-  def call(amount:, from:, to:)
-    Account.transaction do
-      move_money(amount, from, to)
-      persist(from, to)
+  class MoneyMover
+    def initialize(from, to)
+      @from = from
+      @to = to
+    end
+
+    def transfer(amount)
+      @from.balance -= amount
+      @to.balance += amount
+    end
+
+    def commit!
+      @from.save!
+      @to.save!
     end
   end
 
-  private
+  def call(amount:, from:, to:)
+    mover = MoneyMover.new(from, to)
 
-  def move_money(amount, from, to)
-    from.balance -= amount
-    to.balance += amount
-  end
-
-  def persist(*accounts)
-    accounts.each(&:save!)
+    Account.transaction do
+      mover.transfer(amount)
+      mover.commit!
+    end
   end
 end
